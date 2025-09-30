8b9c411bf5da19021a8d927b4f3959a3f7161039
Kavi <koderkavi@gmail.com>
2025-09-30 06:44:36 +0530

    port

--- backend/server.js
+++ backend/server.js
@@ -12,5 +12,5 @@ app.use(express.json());
 app.use("/api/users", authRoutes);
 connectDB();
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on ${PORT}`);
 });
